3a6bfd1da1794a92bd16c88f4389704813526146
topu9872-cpu <topu9872@gmail.com>
2026-04-27 23:46:23 +0600

    added Login with Google and GitHub

--- src/components/HomePage/news/RightSideBar.jsx
+++ src/components/HomePage/news/RightSideBar.jsx
@@ -1,14 +1,30 @@
+'use client'
+import { authClient } from "@/lib/auth-client";
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 const RightSideBar = () => {
+
+  const handleGoogleSingin=async()=>{
+const data = await authClient.signIn.social({
+    provider: "google",
+  });
+  console.log(data)
+  }
+  const handleGitHubSingin=async()=>{
+const data = await authClient.signIn.social({
+    provider: "github",
+  });
+  console.log(data)
+  }
+
   return (
     <div>
       <h2 className=" font-bold text-xl text-center">Login With</h2>
       <div className="flex mt-2 flex-col items-center gap-4">
-        <button className="flex text-nowrap btn text-blue-500 border border-blue-500">
+        <button onClick={handleGoogleSingin} className="flex text-nowrap btn text-blue-500 border border-blue-500">
           <FaGoogle /> Login with Google
         </button>
-        <button className="flex text-nowrap text-blue-500 border border-blue-500  btn">
+        <button onClick={handleGitHubSingin} className="flex text-nowrap text-blue-500 border border-blue-500  btn">
           <FaGithub />
           Login with GitHub
         </button>
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -19,4 +19,17 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+ socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+
+        }, 
+
+          github: { 
+            clientId: process.env.GITHUB_CLIENT_ID , 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET, 
+        }, 
+    },
+
 });
